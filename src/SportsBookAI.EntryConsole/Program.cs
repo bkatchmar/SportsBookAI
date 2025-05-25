577d43601a4fa7820baf21475b4308bd001f06ca
Brian Katchmar <bkatchmar@interactiveedge.com>
2025-05-25 14:22:03 -0400

    Now the base console logic has been ported into the API, we can start using the console project for testing and additional development instead of the main way we get predictions

--- src/SportsBookAI.EntryConsole/Program.cs
+++ src/SportsBookAI.EntryConsole/Program.cs
@@ -1,10 +1,13 @@
 ﻿using System.Globalization;
+using System.Net.NetworkInformation;
+using System.Threading.Tasks;
 using CsvHelper;
 using Microsoft.Extensions.Configuration;
 using SportsBookAI.Core.Classes;
 using SportsBookAI.Core.Interfaces;
 using SportsBookAI.Core.Mongo;
 using SportsBookAI.Core.Mongo.Repositories;
+using SportsBookAI.Core.Structs;
 using SportsBookAI.EntryConsole.SettingsModels;
 
 // Program Start
@@ -54,84 +57,13 @@ if (reposByLeague.Count > 0)
     foreach (KeyValuePair<string, ISportsBookRepository> repos in reposByLeague)
     {
         Console.WriteLine($"League = {repos.Key}\n");
-        IList<ITeam> allTeams = await repos.Value.TeamRepository.GetAllAsync();
-        IList<IMatch> allMatches = await repos.Value.MatchRepository.GetAllAsync();
-        IList<IOverUnder> allOverUnderMarks = await repos.Value.OverUnderRepository.GetAllAsync();
-        IList<IPointSpread> allPointSpreads = await repos.Value.PointSpreadRepository.GetAllAsync();
-        Console.WriteLine($"{repos.Key} Has {allTeams.Count} Teams\n");
-        foreach (ITeam team in allTeams)
-        {
-            Console.WriteLine(team);
-        }
-        Console.WriteLine("");
-        Console.WriteLine($"Stored {allMatches.Count} Matches");
-        Console.WriteLine("");
-        Console.WriteLine("");
-        Console.WriteLine($"Stored {allPointSpreads.Count} Point Spread Records");
-        Console.WriteLine("");
-        Console.WriteLine("===========\n");
 
-        Console.WriteLine("Lets make some preidctions!\n");
+        IAggregator baseAggregatorLeagueData = GetBaseAggregator(repos);
+        await baseAggregatorLeagueData.AggregateAsync();
 
-        IAggregator baseAggregatorLeagueData = new BaseAggregator(repos.Key, repos.Value);
-        baseAggregatorLeagueData.Aggregate();
+        PrintBaseAggregatorStats(baseAggregatorLeagueData);
 
-        IList<IMatch> matchesThatNeedPredictions = allMatches.Where(m => baseAggregatorLeagueData.DoesThisMatchNeedOverUnderPrediction(m)).ToList();
-        Console.WriteLine($"I need to make predictions for {matchesThatNeedPredictions.Count} Matches");
-
-        // Get All necessary pattern repositories, base pattern repo to start off with
-        List<IPatternRepo> allPatternRepos = [new BasePatternRepo(baseAggregatorLeagueData)];
-
-        // TODO: Start looking at possible ways to grab various date range patterns, like a last 14 days, some leagues fair better looking at this range than others
-        if (openingDays != null && openingDays.TryGetValue(repos.Key, out DateTime value))
-        {
-            int daysPassed = (TODAY - value).Days;
-
-            // Put in 7 day lookups for `allPatternRepos`
-            if (daysPassed >= 7)
-            {
-                IAggregator pastSeventDays = new BaseAggregator(repos.Key, repos.Value, TODAY, 7);
-                pastSeventDays.Aggregate();
-                allPatternRepos.Add(new SevenDayRangePatternRepo(pastSeventDays, TODAY));
-            }
-
-            // Put in 14 day lookups for `allPatternRepos`
-            if (daysPassed >= 14)
-            {
-                IAggregator pastFourteenDays = new BaseAggregator(repos.Key, repos.Value, TODAY, 14);
-                pastFourteenDays.Aggregate();
-                // allPatternRepos.Add(new SevenDayRangePatternRepo(pastFourteenDays, TODAY)); // TODO: Make a 14 Day Pattern Repo
-            }
-
-            // Put in 21 day lookups for `allPatternRepos`
-            if (daysPassed >= 21)
-            {
-                IAggregator pastTwentyOneDays = new BaseAggregator(repos.Key, repos.Value, TODAY, 21);
-                pastTwentyOneDays.Aggregate();
-                // allPatternRepos.Add(new SevenDayRangePatternRepo(pastTwentyOneDays, TODAY)); // TODO: Make a 21 Day Pattern Repo
-            }
-        }
-
-        // Collect and write all predictions
-        List<IPredictionPattern> allPredictions = [];
-        foreach (IPatternRepo patterns in allPatternRepos)
-        {
-            // Time to make some predictions
-            IList<IPredictionPattern> currentPredictions = patterns.GetAllPredictions(matchesThatNeedPredictions);
-            foreach (IPredictionPattern pattern in currentPredictions)
-            {
-                Console.WriteLine(pattern.PredictionText);
-            }
-            allPredictions.AddRange(currentPredictions);
-        }
-
-        Console.WriteLine("\nWrite To File\n");
-        if (!string.IsNullOrEmpty(outputSettings?.FileDestinationToWriteTo))
-        {
-            using StreamWriter writer = new(string.Concat(outputSettings?.FileDestinationToWriteTo, repos.Key, " ", "Predictions.csv"));
-            using CsvWriter csv = new(writer, CultureInfo.InvariantCulture);
-            csv.WriteRecords(allPredictions);
-        }
+        Console.WriteLine("==================\n");
     }
 }
 else
@@ -146,4 +78,43 @@ foreach (KeyValuePair<string, ISportsBookRepository> repos in reposByLeague)
     {
         obj.Dispose();
     }
+}
+
+static IAggregator GetBaseAggregator(KeyValuePair<string, ISportsBookRepository> CurrentRepo)
+{
+    return new BaseAggregator(CurrentRepo.Key, CurrentRepo.Value);
+}
+
+static async Task PrintBaseAggregatorStats(IAggregator Aggregator)
+{
+    Console.WriteLine($"Total Over Percentage: {Aggregator.AllOverPercentage.ToString("P2")}");
+    Console.WriteLine($"Total Under Percentage: {Aggregator.AllUnderPercentage.ToString("P2")}");
+    Console.WriteLine($"Total Minus Percentage: {Aggregator.AllMinusSpreadsPercentage.ToString("P2")}");
+    Console.WriteLine($"Total Plus Percentage: {Aggregator.AllPlusSpreadsPercentage.ToString("P2")}\n");
+
+    IList<ITeam> allTeamsFromLeague = await Aggregator.Repo.TeamRepository.GetAllAsync();
+
+    foreach (ITeam team in allTeamsFromLeague)
+    {
+        Console.WriteLine($"Stats For {team}\n");
+
+        if (Aggregator.OversByTeam.TryGetValue(team.TeamName, out int overMarks)) Console.WriteLine($"{overMarks} Over Mark(s)");
+        if (Aggregator.UndersByTeam.TryGetValue(team.TeamName, out int underMarks)) Console.WriteLine($"{underMarks} Under Mark(s)");
+        Console.WriteLine(GetPointSpreadRecordString(team, Aggregator.PointSpreadRecords));
+        Console.WriteLine("\n");
+    }
+}
+
+static string GetPointSpreadRecordString(ITeam TeamForRecord, IDictionary<string, List<PointSpreadRecord>> PointSpreadRecords)
+{
+    List<string> records = [];
+    if (PointSpreadRecords.TryGetValue(TeamForRecord.TeamName, out List<PointSpreadRecord> spreadRecord))
+    {
+        foreach (PointSpreadRecord record in spreadRecord)
+        {
+            records.Add($"{record.Side} Side; {record.Wins} Win(s); {record.Losses} Loss(es)");
+        }
+        return string.Join("\n", [..records]);
+    }
+    return "\n";
 }
